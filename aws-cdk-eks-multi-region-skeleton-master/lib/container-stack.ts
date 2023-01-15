9b7ca7fe6a22c18c3ebf6096bf2542590328c706
Hyunseok Seo <hsseo0501@gmail.com>
2023-01-15 22:29:13 +0900

    feat(cdk): add the k8s manifest files for running kafka based on strimzi

--- aws-cdk-eks-multi-region-skeleton-master/lib/container-stack.ts
+++ aws-cdk-eks-multi-region-skeleton-master/lib/container-stack.ts
@@ -6,5 +6,15 @@ import { Construct } from 'constructs';
 export class ContainerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: EksProps) {
     super(scope, id, props);
+
+    const cluster = props.cluster;
+    const kafkaNamespaceFolder = './yaml-common/kafka/';
+    const kafkaStrimziFolder = `./yaml-${cdk.Stack.of(this).region}/strimzi/`;
+    const kafkaClusterFolder = `./yaml-${cdk.Stack.of(this).region}/kafka/`;
+
+    readYamlFromDir(kafkaNamespaceFolder, cluster);
+    readYamlFromDir(kafkaStrimziFolder, cluster);
+    readYamlFromDir(kafkaClusterFolder, cluster);
+
   }
 }
