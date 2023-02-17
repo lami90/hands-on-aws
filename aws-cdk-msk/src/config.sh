c8186a866dfd40f3757fd92e40de4b43a8d89612
Hyunseok Seo <hsseo0501@gmail.com>
2023-02-17 22:50:01 +0900

    feat(cdk): update ec2 init script to configure files for  SASL/SCRAM protocol

--- aws-cdk-msk/src/config.sh
+++ aws-cdk-msk/src/config.sh
@@ -16,6 +16,19 @@ tar -xzf kafka_2.13-3.3.2.tgz
 # move kafka binary to /opt/kafka
 sudo mv kafka_2.13-3.3.2 /opt/kafka
 
+# copy the truststore from the jre to the kafka config directory
+sudo cp /usr/lib/jvm/jre/lib/security/cacerts /tmp/kafka.client.truststore.jks
+
+# create jaas config file to the kafka config directory
+sudo touch /opt/kafka/config/kafka_client_jaas.conf
+
+# add the dummy value to the jaas config file
+sudo echo "security.protocol=SASL_SSL
+sasl.mechanism=SCRAM-SHA-512
+sasl.jaas.config=org.apache.kafka.common.security.scram.ScramLoginModule required username=\"user\" password=\"user-secret\";
+ssl.truststore.location=/tmp/kafka.client.truststore.jks
+" > /opt/kafka/users_jaas.conf
+
 # environment variables does not work in user-data, so we need to set them in the script
 cd ${HOME}
 
