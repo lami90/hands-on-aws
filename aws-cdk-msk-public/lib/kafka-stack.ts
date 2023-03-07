d44428272306e57dee306e5a128e879b13e2a9e6
Hyunseok Seo <hsseo0501@gmail.com>
2023-03-08 01:48:54 +0900

    feat(cdk): update kafka-stack to delete cluster configuration

--- aws-cdk-msk-public/lib/kafka-stack.ts
+++ aws-cdk-msk-public/lib/kafka-stack.ts
@@ -4,7 +4,6 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 import { VpcStack } from './vpc-stack';
 import { SecretsManagerStack } from './secretsmanager-stack';
-import { CfnConfiguration } from 'aws-cdk-lib/aws-msk';
 
 export class KafkaStack extends cdk.Stack {
     public readonly kafkaCluster: msk.CfnCluster;
@@ -12,34 +11,10 @@ export class KafkaStack extends cdk.Stack {
     constructor(vpcStack: VpcStack, secretsmanager: SecretsManagerStack, scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const config = new CfnConfiguration(this, 'public-kafka-config', {
-            name: 'public-msk-config',
-            serverProperties: 'auto.create.topics.enable=false\n' +
-                'default.replication.factor=3\n' +
-                'min.insync.replicas=2\n' +
-                'num.io.threads=8\n' +
-                'num.network.threads=5\n' +
-                'num.partitions=1\n' +
-                'num.replica.fetchers=2\n' +
-                'replica.lag.time.max.ms=30000\n' +
-                'socket.receive.buffer.bytes=102400\n' +
-                'socket.request.max.bytes=104857600\n' +
-                'socket.send.buffer.bytes=102400\n' +
-                'unclean.leader.election.enable=true\n' +
-                'zookeeper.session.timeout.ms=18000\n' +
-                'allow.everyone.if.no.acl.found=true\n',
-            kafkaVersionsList: ['3.3.1'],
-
-        });
-
         this.kafkaCluster = new msk.CfnCluster(this, 'public-kafka-cluster', {
             clusterName: 'public-kafka-cluster',
             kafkaVersion: '3.3.1',
             numberOfBrokerNodes: 3,
-            configurationInfo: {
-                arn: config.attrArn,
-                revision: 1
-            },
             brokerNodeGroupInfo: {
                 securityGroups: [vpcStack.kafkaSecurityGroup.securityGroupId],
                 clientSubnets: [...vpcStack.vpc.selectSubnets({
@@ -51,11 +26,6 @@ export class KafkaStack extends cdk.Stack {
                         volumeSize: 100
                     }
                 },
-                // connectivityInfo: {
-                //     publicAccess: {
-                //         type: 'SERVICE_PROVIDED_EIPS'
-                //     }
-                // }
             },
             clientAuthentication: {
                 sasl: {
